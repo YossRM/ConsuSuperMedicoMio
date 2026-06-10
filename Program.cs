32844750d7cfe82d01f4bdfc8404586a74e74721
YossRM <yossrm@github.com>
2026-06-10 15:50:34 -0700

    Reestructurar proyecto: mover archivos a raiz para compatibilidad con Railway

--- Program.cs
+++ Program.cs
@@ -62,7 +62,7 @@ app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
-var frontendPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "frontend");
+var frontendPath = Path.Combine(Directory.GetCurrentDirectory(), "frontend");
 app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(frontendPath) });
 app.UseStaticFiles(new StaticFileOptions { FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(frontendPath) });
 
